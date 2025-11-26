2f337849103c035dcfada69f30de6b6e1ff240ba
Aditya Mishra <adimis.ai.001@gmail.com>
2025-11-26 22:17:39 +0530

    update src/charts/index.ts

--- src/charts/index.ts
+++ src/charts/index.ts
@@ -3,9 +3,9 @@ export * from "./types";
 export * from "./utils";
 
 // Chart Components
-export { default as AreaChart, AreaChartCard } from "./AreaChart";
-export { default as BarChart, BarChartCard } from "./BarChart";
-export { default as LineChart, LineChartCard } from "./LineChart";
-export { default as PieChart, PieChartCard } from "./PieChart";
-export { default as RadarChart, RadarChartCard } from "./RadarChart";
-export { default as RadialChart, RadialChartCard } from "./RadialChart";
+export { AreaChart, AreaChartCard } from "./AreaChart";
+export { BarChart, BarChartCard } from "./BarChart";
+export { LineChart, LineChartCard } from "./LineChart";
+export { PieChart, PieChartCard } from "./PieChart";
+export { RadarChart, RadarChartCard } from "./RadarChart";
+export { RadialChart, RadialChartCard } from "./RadialChart";
